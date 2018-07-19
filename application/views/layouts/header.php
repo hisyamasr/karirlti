65ce3418392f005ed674664292dc848e0a597c97
Andi <andi.sugiarto@len.co.id>
2018-07-19 10:25:20 +0700

    penambahan redirect

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>	
-	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap/bootstrap.css"/>
-	<link rel="stylesheet" type="text/css" href="assets/css/datepicker/bootstrap-datepicker3.css"/>	
-	<link rel="stylesheet" type="text/css" href="assets/css/jquery-ui.min.css"/>
-	<link rel="stylesheet" type="text/css" href="assets/css/global.css"/>
-	<link rel="stylesheet" href="assets/fontawesome-5.1/css/all.css">
-	<script type="text/javascript" src='assets/js/jquery-3.3.1.js'/></script>
-	<script type="text/javascript" src='assets/js/jquery-ui.js'/></script>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/bootstrap/bootstrap.css"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/datepicker/bootstrap-datepicker3.css"/>	
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/jquery-ui.min.css"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/global.css"/>
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/fontawesome-5.1/css/all.css">
+	<script type="text/javascript" src='<?php echo base_url();?>assets/js/jquery-3.3.1.js'/></script>
+	<script type="text/javascript" src='<?php echo base_url();?>assets/js/jquery-ui.js'/></script>
+	<script type="text/javascript" src='<?php echo base_url();?>assets/js/jquery-redirect.js'/></script>
 	<!-- <script type="text/javascript" src='assets/js/popper.1.14.js'/></script> -->
-	<script type="text/javascript" src='assets/js/bootstrap.min.js'/></script>
-	<script type="text/javascript" src='assets/js/bootstrap-datepicker.min.js'/></script>
-	<script type="text/javascript" src='assets/js/bootstrap-datepicker.id.min.js'/></script>
+	<script type="text/javascript" src='<?php echo base_url();?>assets/js/bootstrap.min.js'/></script>
+	<script type="text/javascript" src='<?php echo base_url();?>assets/js/bootstrap-datepicker.min.js'/></script>
+	<script type="text/javascript" src='<?php echo base_url();?>assets/js/bootstrap-datepicker.id.min.js'/></script>
 	<meta charset="utf-8">
 
 	<style type="text/css">
@@ -88,13 +89,13 @@
 <body>
 	<nav class="navbar navbar-light bg-light mb-3">
 		<a class="navbar-brand" href="https://www.len-telko.co.id" style="margin-left:30px;">
-			<img src="assets/images/cropped-LTI-2.png" width="150" height="56" class="d-inline-block align-top" alt="">
+			<img src="<?php echo base_url();?>assets/images/cropped-LTI-2.png" width="150" height="56" class="d-inline-block align-top" alt="">
 		</a>
 	</nav>
 	<div id="warning" title="Peringatan !" style="display:none;">
 		<br />
 		<div class="col-md-2">
-			<img src="assets/images/alert.png" height="42" width="42">
+			<img src="<?php echo base_url();?>assets/images/alert.png" height="42" width="42">
 		</div>
 		<div class="col-md-10" style="padding-left:3em"><span id="isiWarning"></span></div>
 	</div>
